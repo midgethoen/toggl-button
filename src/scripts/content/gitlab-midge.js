c2efda0faabb57c0246a4562ce92e0c40a314623
Midge 't Hoen <midgethoen@gmail.com>
2018-05-15 15:25:51 +0200

    add custom gitlab profile

---
+++ src/scripts/content/gitlab-midge.js
@@ -0,0 +1,76 @@
+/*jslint indent: 2, unparam: true*/
+/*global $: false, document: false, togglbutton: false*/
+
+'use strict';
+
+togglbutton.render(
+  '.issue-details .detail-page-description:not(.toggl)',
+  { observe: true },
+  function(elem) {
+    var link,
+      description,
+      numElem =
+        $('.identifier') ||
+        $('.breadcrumbs-list li:last-child .breadcrumbs-sub-title'),
+      crossProjectReferenceElem = $('.cross-project-reference cite'),
+      titleElem = $('.title', elem),
+      projectElem =
+        $('.title .project-item-select-holder') ||
+        $('.breadcrumbs-list li:nth-last-child(3) .breadcrumb-item-text'),
+      actionsElem = $('.detail-page-header-actions');
+    description = titleElem.textContent.trim();
+    // if (numElem !== null) {
+    //   description = numElem.textContent.split(" ").pop().trim() + " " + description;
+    // }
+    if (crossProjectReferenceElem !== null) {
+      description = crossProjectReferenceElem.textContent + ' ' + description;
+    }
+
+    link = togglbutton.createTimerLink({
+      className: 'gitlab-midge',
+      description: description,
+      projectName: projectElem.textContent
+    });
+
+    actionsElem.parentElement.insertBefore(link, actionsElem);
+  }
+);
+
+togglbutton.render(
+  '.merge-request-details .detail-page-description:not(.toggl)',
+  { observe: true },
+  function(elem) {
+    var link,
+      description,
+      numElem =
+        $('.identifier') ||
+        $('.breadcrumbs-list li:last-child .breadcrumbs-sub-title'),
+      titleElem = $('.title', elem),
+      projectElem =
+        $('.title .project-item-select-holder') ||
+        $('.breadcrumbs-list li:nth-last-child(3) .breadcrumb-item-text'),
+      actionsElem = $('.detail-page-header-actions');
+
+    description = titleElem.textContent.trim();
+
+    if (numElem !== null) {
+      description =
+        'MR' +
+        numElem.textContent
+          .split(' ')
+          .pop()
+          .trim()
+          .replace('!', '') +
+        '::' +
+        description;
+    }
+
+    link = togglbutton.createTimerLink({
+      className: 'gitlab-midge',
+      description: description,
+      projectName: projectElem.textContent
+    });
+
+    actionsElem.parentElement.insertBefore(link, actionsElem);
+  }
+);
